2ce2211f526b765eb2371fe27c9f6a8f08c45489
Ansel Halliburton <github@anseljh.com>
2016-06-09 01:36:37 -0700

    Add abstract ID extractor regex

--- ssrn_urls.py
+++ ssrn_urls.py
@@ -1,3 +1,6 @@
+import re
+
 SSRN_AUTHOR_URL = "http://papers.ssrn.com/sol3/cf_dev/AbsByAuth.cfm?per_id=%s"
 SSRN_ABSTRACT_URL_LONG = "http://papers.ssrn.com/sol3/papers.cfm?abstract_id=%s"
 SSRN_ABSTRACT_URL_SHORT = "http://ssrn.com/abstract=%s"
+ID_EXTRACTOR_SHORT_RE = re.compile(r"http\:\/\/(papers\.)?ssrn\.com\/abstract\=(\d+)")
